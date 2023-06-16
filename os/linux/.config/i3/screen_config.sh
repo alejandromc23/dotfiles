994d698d2c7171dba636968bf4a63c90c3815f89
Alejandro Mascort Colomer <alejandro.mascort17@hotmail.com>
2023-06-16 07:54:23 +0200

    add vertical screen config

--- os/linux/.config/i3/screen_config.sh
+++ os/linux/.config/i3/screen_config.sh
@@ -7,7 +7,7 @@ xrandr_output=$(xrandr --query)
 if [[ $xrandr_output == *"DP-1 connected"* && $xrandr_output == *"DP-2 connected"* ]]; then
     xrandr --output DP-1 --mode 1920x1080 --pos 0x0 \
            --output DP-2 --mode 2560x1080 --pos 1920x0 --primary \
-           --output eDP-1 --mode 1920x1080 --pos 4480x0
+           --output eDP-1 --mode 1920x1080 --pos 1920x1080
 fi
 
 # set wallpaper

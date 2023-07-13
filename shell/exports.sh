be8c5a86e34b9aa7c168d73c1f14341ef2d7e264
Alejandro Mascort Colomer <alejandro.mascort17@hotmail.com>
2023-07-13 19:06:38 +0200

    add go envs & include into path

--- shell/exports.sh
+++ shell/exports.sh
@@ -6,4 +6,6 @@ export PATH="$PATH:$PNPM_HOME"
 # pnpm end
 
 # Go path to bin executables
-export PATH="$PATH:/usr/local/go/bin"
+export GOPATH="$HOME/go"
+export GOBIN="$GOPATH/bin"
+export PATH="$PATH:/usr/local/go/bin:$GOBIN"

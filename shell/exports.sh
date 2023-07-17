2ea9b2dce0da232c4c2bae936e8c5dc8ef6781d7
Alejandro Mascort Colomer <alejandro.mascort17@hotmail.com>
2023-07-17 21:04:49 +0200

    add nvm export

--- shell/exports.sh
+++ shell/exports.sh
@@ -9,3 +9,9 @@ export PATH="$PATH:$PNPM_HOME"
 export GOPATH="$HOME/go"
 export GOBIN="$GOPATH/bin"
 export PATH="$PATH:/usr/local/go/bin:$GOBIN"
+
+# Node version manager
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
